3ea382160bdb5ddbc70c6bd44cacab4d62faee08
akashbokde98 <akashbokde98@gmail.com>
2023-04-21 19:26:45 +0530

    Added:Welcome Message

---
+++ src/main/java/com/bl/addressbooksystem/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bl.addressbooksystem;
+
+public class AddressBookMain
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome To Address Book System Program.");
+    }
+}
