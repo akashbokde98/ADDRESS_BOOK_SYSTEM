9b2b29c99f66d07d05c6f755211c80b638d341c9
akashbokde98 <akashbokde98@gmail.com>
2023-04-21 19:57:19 +0530

    Added:Created Contacts in Address Book

--- src/main/java/com/bl/addressbooksystem/AddressBookMain.java
+++ src/main/java/com/bl/addressbooksystem/AddressBookMain.java
@@ -5,5 +5,8 @@ public class AddressBookMain
     public static void main(String[] args)
     {
         System.out.println("Welcome To Address Book System Program.");
+        Contacts obj = new Contacts("Akash", "Kumar", "Mahatma Gandhi Square", "Raipur", "Chattisgarh", 491228, 8830584633L, "akashbokde98@gmail.com");
+        System.out.println(obj);
     }
 }
+
